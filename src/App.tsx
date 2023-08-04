3ea7745ca1aa05fb56d4aec695bd48a4d2697f20
unknown <johnmsn22@gmail.com>
2023-08-04 11:53:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className="app bg-gray-20">App</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "gray-20": "#f8f4eb",
+        "gray-50": "#efe6e6",
+        "gray-100": "#dfcccc",
+        "gray-500": "#5e0000",
+        "primary-100": "#ffe1e0",
+        "primary-300": "#ffa6a3",
+        "primary-500": "#ff6b66",
+        "secondary-400": "#ffcd58",
+        "secondary-500": "#ffc132",
+      },
+      backgroundImage:(theme)=>({
+        "gradient-yellowred": "linear-gradient(90deg, #ff616a 0%, #ffc837 100%)",
+        "mobile-home": "url('./assets/images/HomePageGraphic.png')",
+      }),
+      fontFamily:{
+        dmsans:["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"]
+      },
+      content:{
+        evolvetext: "url('./assets/images/EvolveText.png')",
+        abstrsctwaves: "url('./assets/images/AbstractWaves.png')",
+        sparkles: "url('./assets/images/Sparkles.png')",
+        circles: "url('./assets/images/Circles.png')",
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    }
+  },
+  plugins: [], 
+}
