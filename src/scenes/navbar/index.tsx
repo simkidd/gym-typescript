2d1927a1032fa8a3e00ccc986b3555b4e58adfdf
unknown <johnmsn22@gmail.com>
2023-08-06 08:56:20 +0100

    [Added]: classes section

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -16,7 +16,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
     const flexBetween = "flex items-center justify-between"
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
-    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow py-4"
 
     return (
         <nav>
